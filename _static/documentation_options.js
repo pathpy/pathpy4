cc1989a60b184b9ecbe253294e3f95842f6fc9d3
GitHub Action <action@github.com>
2022-01-24 16:27:29 +0000

    Add changes for cc8f7a0322cc0aa94f66f8d696d8293e805175fe

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.24.ged17971',
+    VERSION: '4.0.0+1.gcc8f7a0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
